c68736c07a04c5515b68d30db85fce5bf2d011c8
Max0Power <53047961+Max0Power@users.noreply.github.com>
2021-04-11 07:04:44 +0300

    Unitylla tehdyt shaderit pushattu

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Lopettaa ohjelman painamalla Esc -painiketta
+/// </summary>
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
